c6d057d7e8024a35539414b7f42c2abb99d82ed3
Krzysztof Joachimiak <kjoa.92@gmail.com>
2020-10-23 18:31:58 +0200

    Installation script

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # R on Adroid
 
-## Linux libs to install
-libtiff 
-pango
+## Installation
+
+1. Install [termux](https://play.google.com/store/apps/details?id=com.termux&hl=pl).
+2. Open Termux App.
+3. Run following commands:
+
+```
+pkg install curl gnupg
+curl https://github.com/RonMobile/r-on-android/blob/master/install.sh
+sh install.sh
+```
---
+++ install_r.sh
@@ -0,0 +1,41 @@
+#!/bin/sh 
+
+mkdir -p $PREFIX/etc/apt/sources.list.d/
+echo "deb https://its-pointless.github.io/files/24 termux extras" > "$PREFIX/etc/apt/sources.list.d/"
+echo "deb https://its-pointless.github.io/files/24 termux extras" > "$PREFIX/etc/apt/sources.list.d/pointless.list"
+deb https://its-pointless.github.io/files/24 termux extras
+curl "https://its-pointless.github.io/pointless.gpg" | apt-key add
+pkg update
+pkg install r-base \
+            make \
+            clang \
+            gcc-9 \
+            libgfortran \
+            openssl \
+            libcurl \
+            libicu \
+            libxml2 \
+            zlib-dev\
+            ndk-sysroot \
+            pkg-config \
+            cmake \
+            git \
+            libcairo \
+            libtiff \
+            pango \
+             
+apt-add-repository
+
+# copy gfortran
+cd /data/data/com.termux/files/usr/bin
+cp gfortran-9 gfortran
+
+# install LightGBM
+git clone --recursive https://github.com/microsoft/LightGBM
+cd LightGBM/
+mkdir build
+cd build
+cmake ..
+make -j4
+cd ..
+Rscript build_r.R 
