1c929b279d9406901344e4be10375b85e073e933
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2021-02-26 08:16:33 +0100

    Update

--- install_r.sh
+++ install_r.sh
@@ -10,13 +10,14 @@
 
 curl -LO https://its-pointless.github.io/setup-pointless-repo.sh
 bash setup-pointless-repo.sh
-pkg install r-base make clang gcc-9 libgfortran openssl libcurl libicu libxml2 ndk-sysroot pkg-config cmake git libcairo libtiff pango zlib
-             
+pkg install r-base make clang gcc-9  openssl libcurl libicu libxml2 ndk-sysroot pkg-config cmake git libcairo libtiff pango zlib #libgfortran
 apt-add-repository
 
 # copy gfortran
 cd /data/data/com.termux/files/usr/bin
 cp gfortran-9 gfortran
+
+setupclang-gfort-9
 
 # install LightGBM
 # git clone --recursive https://github.com/microsoft/LightGBM
