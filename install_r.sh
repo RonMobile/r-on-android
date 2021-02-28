d1c3df9e52e1a9a71d445e91752060e40d07884a
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2021-02-28 21:24:03 +0100

    Update install_r.sh

--- install_r.sh
+++ install_r.sh
@@ -1,17 +1,8 @@
 #!/bin/sh 
 
-# pkg up
-# mkdir -p $PREFIX/etc/apt/sources.list.d/
-# echo "deb https://its-pointless.github.io/files/24 termux extras" > "$PREFIX/etc/apt/sources.list.d/"
-# echo "deb https://its-pointless.github.io/files/24 termux extras" > "$PREFIX/etc/apt/sources.list.d/pointless.list"
-
-# curl "https://its-pointless.github.io/pointless.gpg" | apt-key add
-# pkg up
-
 curl -LO https://its-pointless.github.io/setup-pointless-repo.sh
 bash setup-pointless-repo.sh
 pkg install r-base make clang gcc-9  openssl libcurl libicu libxml2 ndk-sysroot pkg-config cmake git libcairo libtiff pango zlib #libgfortran
-apt-add-repository
 
 # copy gfortran
 cd /data/data/com.termux/files/usr/bin
